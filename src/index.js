c5aeb106cb34c6fe81e7af776010d69803aac816
mr.ginger <tkg78912@gmail.com>
2021-08-18 15:12:04 +0900

    feat : vanilaJS & Redux Counter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
+import { createStore } from "redux";
 
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const ACTION = {
+  countPlus: "plus",
+  countMinus: "minus",
+};
+
+number.innerText = 0;
+
+const { countPlus, countMinus } = ACTION;
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case countPlus:
+      return state + 1;
+    case countMinus:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const onChange = () => {
+  number.innerText = store.getState();
+};
+
+store.subscribe(onChange);
+
+plus.addEventListener("click", () => store.dispatch({ type: countPlus }));
+minus.addEventListener("click", () => store.dispatch({ type: countMinus }));
